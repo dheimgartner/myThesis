1bd4f078c9c5de280fab85fdefbbcc05d87bcc5e
daniehei <daniel.heimgartner@ivt.baug.ethz.ch>
2025-01-07 12:00:19 +0100

    tmp _build dir

--- R/make.R
+++ R/make.R
@@ -1,11 +1,19 @@
 make <- function(dir = "thesis", file = "thesis.Rnw", remove_build = TRUE) {
+  BUILD_DIR <- "_build"
   if (remove_build) {
     unlink("build", recursive = TRUE)
   }
 
+  tmp_build_dir <- function(src = dir, build_dir = BUILD_DIR) {
+    dir.create(build_dir)
+    file.copy(src, build_dir, recursive = TRUE)
+    paste(build_dir, src, sep = "/")
+  }
+
+  tmp_dir <- tmp_build_dir()
   wd <- getwd()
   o <- options()
-  setwd(dir)
+  setwd(tmp_dir)
 
   set_latexpath()
   check_latexmk()
@@ -15,9 +23,9 @@ make <- function(dir = "thesis", file = "thesis.Rnw", remove_build = TRUE) {
   system("make", ignore.stderr = TRUE)
   remove_aux()
 
-  file.copy("build", "..", recursive = TRUE)
-  unlink("build", recursive = TRUE)
+  file.copy("build", "../..", recursive = TRUE)
   setwd(wd)
+  unlink(BUILD_DIR, recursive = TRUE)
   options(o)
 
   invisible(1)
