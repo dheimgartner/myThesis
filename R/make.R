e210daf609a9793b2b37299b733792d661b88af3
daniehei <daniel.heimgartner@ivt.baug.ethz.ch>
2025-01-07 10:50:14 +0100

    path handling

--- R/make.R
+++ R/make.R
@@ -1,17 +1,23 @@
 make <- function(dir = "thesis", file = "thesis.Rnw", remove_build = TRUE) {
+  if (remove_build) {
+    unlink("build")
+  }
+
   wd <- getwd()
   o <- options()
   setwd(dir)
 
-  if (remove_build) {
-    unlink("build")
-  }
+  set_latexpath()
+  check_latexmk()
+  check_pdflatex()
 
   Sweave(file)
   system("make")
   remove_aux()
 
+  file.copy("build", "..", recursive = TRUE)
   setwd(wd)
   options(o)
+
   invisible(1)
 }
--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,26 @@ remove_aux <- function() {
   cmd <- "find . -type f -name '*.aux' -delete"
   system(cmd)
 }
+
+set_latexpath <- function(path = "~/.local/bin/") {
+  if (!is.null(getOption("__latexpath"))) {
+    path <- getOption("__latexpath")
+  } else {
+    options("__latexpath" = path)
+  }
+  Sys.setenv(PATH = paste(Sys.getenv("PATH"), getOption("__latexpath"), sep = ":"))
+}
+
+check_latexmk <- function() {
+  tryCatch(
+    system("latexmk --version"),
+    warning = function(w) stop("Make sure the correct path is set in latexmk()")
+  )
+}
+
+check_pdflatex <- function() {
+  tryCatch(
+    system("pdflatex --version"),
+    warning = function(w) stop("Make sure the correct path is set in latexmk()")
+  )
+}
