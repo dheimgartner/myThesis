9b209352277270762d342d48d15ccd1e913682ca
daniehei <daniel.heimgartner@ivt.baug.ethz.ch>
2025-03-12 18:40:53 +0100

    blob

---
+++ vignettes/thesis.R
@@ -0,0 +1,18 @@
+### R code from vignette source 'thesis.Rnw'
+
+###################################################
+### code chunk number 1: preliminaries
+###################################################
+## add libraries here
+
+## maybe adjust width here...
+options(prompt = "R> ", continue = "+  ", width = 70, useFancyQuotes = FALSE,
+        digits = 3)
+
+
+###################################################
+### code chunk number 2: main.Rnw:8-9
+###################################################
+head(iris)
+
+
