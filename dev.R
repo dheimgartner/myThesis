273addfc008a360a64b42890ac3c1c9793e4f1a7
daniehei <daniel.heimgartner@ivt.baug.ethz.ch>
2025-03-21 22:00:26 +0100

    baaam - nice coef table for mmdcev

--- dev.R
+++ dev.R
@@ -3,3 +3,9 @@ x <- c(1.00, 1.001, -1.1)
 x_ <- format(x, justify = "left")  # left is default
 stars <- c("", "*", "***")
 paste0(x_, stars)
+
+
+## kableExtra footnotes
+library(kableExtra)
+kbl(head(iris), format = "latex") %>%
+  kableExtra::footnote("foo")
